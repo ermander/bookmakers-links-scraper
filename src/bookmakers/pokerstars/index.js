8b04e6fd0a3c1000d547f5ddeea2d751d9779eae
Emanuele Bertuol <61063909+ermander@users.noreply.github.com>
2022-07-13 23:34:58 +0200

    Completata la funzione per prendere gli id dei campionati di calcio, tennis, basketball e volleryball

--- src/bookmakers/pokerstars/index.js
+++ src/bookmakers/pokerstars/index.js
@@ -1,19 +1,36 @@
+const urls = require("./urls.json")
+
 async function pokerstarsLinks(driver, By, until) {
+  const competitions = {
+    soccer: [],
+    tennis: [],
+    basketball: [],
+    volleyball: [],
+  }
+
   await driver.get("https://www.pokerstarssports.it/#/soccer/competitions")
   await driver.findElement(By.id("onetrust-accept-btn-handler")).click()
 
-  const links = await driver.wait(
-    until.elementsLocated(By.className("competitionContent__item__link"))
-  )
-  const competitions = []
+  for (const url of Object.values(urls)) {
+    await driver.get(url)
 
-  for (const link of links) {
-    const competitionInfoes = await link.getAttribute("innerText")
-    competitions.push({
-      competitionId: await link.getAttribute("data-leagueid"),
-      tournament: competitionInfoes.trim(),
-    })
+    const sportName = url.split("/")[4].split("/")[0]
+
+    const links = await driver.wait(
+      until.elementsLocated(By.className("competitionContent__item__link"))
+    )
+
+    for (const link of links) {
+      const competitionInfoes = await link.getAttribute("innerText")
+      competitions[`${sportName}`].push({
+        competitionId: await link.getAttribute("data-leagueid"),
+        tournament: competitionInfoes.trim(),
+      })
+    }
   }
+
+  // Post to API
+  return
 }
 
 module.exports = pokerstarsLinks
